37d110c39e266d8f2c68f8be36fff676884c2a98
“guohaojie” <1172489638@qq.com>
2021-07-13 14:51:13 +0800

    111

---
+++ src/test/java/com/example/practice_design_pattern/PracticeDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_design_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
